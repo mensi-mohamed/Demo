f3068cf96b449a91b53e9c07a5bf801e5a9f0293
mensi_mohamed <mensi.mohamed@hotmail.com>
2017-01-22 14:29:49 +0100

    Lien + route + appel API"

--- Demo/src/Demo/TestBundle/Controller/DefaultController.php
+++ Demo/src/Demo/TestBundle/Controller/DefaultController.php
@@ -4,12 +4,16 @@ namespace Demo\TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use GuzzleHttp\Client;
 
 class DefaultController extends Controller {
 
     public function indexAction() {
         $user = $this->getUser();
         if ($user) {
+            //$ch = curl_init();
+            //curl_setopt($ch, CURLOPT_URL, 'http://api.openweathermap.org/data/2.5/weather?q=paris&appid=ed56a35e6f32fc8b4608a6a568f3d9f4');
+            //$result = curl_exec($ch);
             return $this->render('DemoTestBundle:Default:index.html.twig');
         }
         return new RedirectResponse($this->generateUrl('fos_user_security_login'));
